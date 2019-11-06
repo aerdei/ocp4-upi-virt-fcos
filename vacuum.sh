dab4a335f23dea7410393c4ae21a3629707b9c6c
aerdei <aerdei@redhat.com>
2019-11-06 12:02:57 +0100

    Restructuring repo

--- vacuum.sh
+++
@@ -1,6 +0,0 @@
-scp root@192.168.122.53:/var/named/{kahvi.online.zone,155.168.192.in-addr.arpa} ./named/var/named/
-scp root@192.168.122.53:/etc/named.conf ./named/etc/named.conf
-scp root@192.168.122.53:/etc/dhcp/dhcpd.conf dhcpd/etc/dhcpd/dhcpd.conf
-scp root@192.168.122.53:/etc/httpd/conf/httpd.conf ./etc/httpd/conf/
-scp root@192.168.122.53:/etc/haproxy/haproxy.cfg ./haproxy/etc/haproxy/haproxy.cfg
-scp root@192.168.122.53:/etc/chrony.conf ./chrony/etc/chrony/chrony.conf
